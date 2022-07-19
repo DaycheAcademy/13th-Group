88e646f1059c8a431bc4095323603856cd83eb3b
m.esmaeilian <mesmaeilian2020@gmail.com>
2022-07-19 20:10:55 +0430

    Main Directory of exercises

---
+++ MehdiEsmaeilian/hopWiz.py
@@ -0,0 +1,12 @@
+
+# hop wiz game
+
+for number in range(1, 100):
+        if number % 15 == 0:
+            print('hopeWIZ')
+        elif number % 3 == 0:
+            print('WIZ')
+        elif number % 5 == 0:
+            print('hope')
+        else:
+            print(number)
