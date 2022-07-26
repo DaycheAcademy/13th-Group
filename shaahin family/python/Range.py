dd6d7d593d0227df8b95c87c2fca175d2d90415d
shaahinf88 <shaahinf88@gmail.com>
2022-07-26 11:17:45 +0430

    range char

--- shaahin family/python/Range.py
+++ shaahin family/python/Range.py
@@ -1,5 +1,5 @@
 def rng(min_, max_, step):
-    while x:
+    while True:
         if min_ < max_ - step:
             yield round(min_, 2)
             min_ += step
@@ -8,6 +8,19 @@ def rng(min_, max_, step):
             break
 
 
+def rng_char(start, stop, step):
+
+    for i in range(ord(start), ord(stop) + 1, step):
+        yield chr(i)
+
+
 if __name__ == "__main__":
+    print("new range(rng):")
+
     for i in rng(2.3, 3.78, 0.01):
         print(i)
+    print("*" * 30)
+    print("new range(rng_char):")
+
+    for m in rng_char('a', 'm', 2):
+        print(m)
